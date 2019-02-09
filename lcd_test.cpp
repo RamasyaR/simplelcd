5feee06998d4f53756b58124fa003935ad48ab32
Roman <36107602+RamasyaR@users.noreply.github.com>
2019-02-09 17:15:01 +0300

    Update lcd_test.cpp

--- lcd_test.cpp
+++ lcd_test.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-int i = 0;
 volatile sig_atomic_t flag = 0;
 void my_function(int sig){ 
 	flag = 1;
@@ -13,54 +12,16 @@ void my_function(int sig){
 int main (void){
 	signal(SIGINT, my_function);
  
-	GPIOClass* gpio228 = new GPIOClass("228");
-	(*gpio228).setdir_gpio("out");
-	GPIOClass* gpio117 = new GPIOClass("117");
-	(*gpio117).setdir_gpio("out");
-	GPIOClass* gpio118 = new GPIOClass("118");
-	(*gpio118).setdir_gpio("out");
-	GPIOClass* gpio73 = new GPIOClass("73");
-	(*gpio73).setdir_gpio("out");
-	GPIOClass* gpio72 = new GPIOClass("72");
-	(*gpio72).setdir_gpio("out");
-	GPIOClass* gpio71 = new GPIOClass("71");
-	(*gpio71).setdir_gpio("out");
-	
-	//EN RS D4 D5 D6 D7
-	lcdPins lcd {*gpio228, *gpio117, *gpio118, *gpio73, *gpio72, *gpio71};
- 
-	Lcd_Start(lcd);
-	Lcd_Clear(lcd);
-	Lcd_Set_Cursor(lcd,1,5);
-	Lcd_Print_String(lcd,"Hello");
-	usleep(10000000);
+	simplelcd lcd("228","117","118","73","72","71");	//EN RS D4 D5 D6 D7
+
+	lcd.start();
+	lcd.clear();
+	lcd.setCursor(1,3);
+	lcd.printString("Hello World");
 	while(!flag){
-		Lcd_Clear(lcd);
-		Lcd_Set_Cursor(lcd,1,3);
-		Lcd_Print_String(lcd,"Don't worry");
-		Lcd_Set_Cursor(lcd,2,4);
-		Lcd_Print_String(lcd,"Be happy!");
+		lcd.setCursor(2,4);
+		lcd.printString("Don't worry");
 		usleep(1000000);
 	}
-
-	/*(*gpio228).setval_gpio("0");
- 	delete gpio228;
- 	gpio228 = NULL;
-	(*gpio117).setval_gpio("0");
- 	delete gpio117;
- 	gpio117 = NULL;
-	(*gpio118).setval_gpio("0");
- 	delete gpio118;
- 	gpio118 = NULL;
-	(*gpio73).setval_gpio("0");
- 	delete gpio73;
- 	gpio73 = NULL;
-	(*gpio72).setval_gpio("0");
- 	delete gpio72;
- 	gpio72 = NULL;
-	(*gpio71).setval_gpio("0");
-	delete gpio71;
- 	gpio71 = NULL;*/
-    
-	return 0;
+    	return 0;
 }
